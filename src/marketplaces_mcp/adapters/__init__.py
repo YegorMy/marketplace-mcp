1a14cae02e315406e285a3836a0c36eb80be9e1d
Tomolot69 <fedor.tvgu@gmail.com>
2026-07-22 11:09:17 +0300

    feat: register new marketplace adapters

--- src/marketplaces_mcp/adapters/__init__.py
+++ src/marketplaces_mcp/adapters/__init__.py
@@ -1,5 +1,7 @@
+from .avito import AvitoAdapter
 from .base import BaseAdapter
 from .ozon import OzonAdapter
+from .wildberries import WildberriesAdapter
 from .yandex_market import YandexMarketAdapter
 
-__all__ = ["BaseAdapter", "OzonAdapter", "YandexMarketAdapter"]
+__all__ = ["AvitoAdapter", "BaseAdapter", "OzonAdapter", "WildberriesAdapter", "YandexMarketAdapter"]
